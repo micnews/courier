d9e85a7851b44e041f54d0667197c26d3b00f07b
Roilan <roilan14@gmail.com>
2016-05-09 15:48:49 -0400

    wip render / compile w/ test

---
+++ src/index.js
@@ -0,0 +1,43 @@
+import { createWriteStream } from 'fs';
+import { prettyPrint } from 'html';
+import mailchimpify from 'mailchimpify';
+import assert from 'assert';
+import { injectReactEmailAttributes, renderEmail } from 'react-html-email';
+
+injectReactEmailAttributes();
+
+function assertTemplateParams (templateName, props) {
+  assert(templateName, '`templateName` should be passed in to `render`.');
+  assert(typeof (templateName) === 'string', '`templateName` should be a string');
+  assert(typeof (props) === 'object' && !Array.isArray(props), '`props` should be an object');
+}
+
+const renderHtml = (template) => prettyPrint(mailchimpify(renderEmail(template)), { indent_size: 2 });
+
+export default (opts) => {
+  assert(opts, 'Requires an `opts` object');
+  assert(typeof (opts) === 'object', '`opts` should be an object');
+  assert(opts.allTemplates, 'Requires an `allTemplates` object in opts');
+  assert(typeof (opts.allTemplates) === 'object' && !Array.isArray(opts.allTemplates), '`allTemplates` should be an object');
+
+  const { allTemplates } = opts;
+
+  return {
+    templateNames: () => Object.keys(allTemplates).map(template => template).join(', '),
+    render: (templateName, props = {}) => {
+      assertTemplateParams(templateName, props);
+      const { template } = allTemplates[templateName];
+      const html = renderHtml(template(props));
+
+      return html;
+    },
+    compile: (templateName, props = {}, dir = __dirname) => {
+      assertTemplateParams(templateName, props);
+      const { template, fileName } = allTemplates[templateName];
+      const stream = createWriteStream(`${dir}/${fileName}.html`);
+      const html = renderHtml(template(props));
+
+      stream.once('open', () => stream.end(html));
+    }
+  };
+};
---
+++ test/index.js
@@ -0,0 +1,152 @@
+import test from 'tape';
+import courier from '../src/index';
+import React from 'react';
+import { Item, Email, Span } from 'react-html-email';
+
+test('throw without opts', (t) => {
+  const message = 'Should throw if no opts passed in';
+
+  t.throws(() => courier(), message);
+  t.end();
+});
+
+test(`throw if opts isn't an object`, (t) => {
+  const message = `Should throw if opts isn't an object`;
+  const opts = [];
+
+  t.throws(() => courier(opts), message);
+  t.end();
+});
+
+test('throw with no templates', (t) => {
+  const message = `Should throw if 'allTemplates' isn't passed in`;
+  const opts = {};
+
+  t.throws(() => courier(opts), message);
+  t.end();
+});
+
+test(`throws if templates isn't an object`, (t) => {
+  const message = `Should throw if 'allTemplates' isn't an object`;
+  const opts = {
+    allTemplates: ''
+  };
+
+  t.throws(() => courier(opts), message);
+  t.end();
+});
+
+test(`templateNames should return an object of template names`, (t) => {
+  const opts = {
+    allTemplates: {
+      myTemplate: {
+        template: `<p>Just a paragraph</p>`,
+        fileName: 'myTemplate'
+      },
+      yourTemplate: {
+        template: `<span>Just span</span>`,
+        fileName: 'yourTemplate'
+      }
+    }
+  };
+  const { templateNames } = courier(opts);
+  const expected = 'myTemplate, yourTemplate';
+
+  t.equal(templateNames(), expected);
+  t.end();
+});
+
+test(`render should throw if no templateName is passed`, (t) => {
+  const message = `throw without templateName in render`;
+  const opts = {
+    allTemplates: {
+      myTemplate: {
+        template: `<p>foo</p>`,
+        fileName: 'myTemplate'
+      }
+    }
+  };
+  const { render } = courier(opts);
+
+  t.throws(() => render(), message);
+  t.end();
+});
+
+test(`render should throw if templateName is not a string`, (t) => {
+  const message = `throw without a valid templateName`;
+  const opts = {
+    allTemplates: {
+      myTemplate: {
+        template: `<p>foo</p>`,
+        fileName: 'myTemplate'
+      }
+    }
+  };
+  const { render } = courier(opts);
+
+  t.throws(() => render([]), message);
+  t.end();
+});
+
+test(`render should throw if props is not an object`, (t) => {
+  const message = `throw if props is not an object`;
+  const opts = {
+    allTemplates: {
+      myTemplate: {
+        template: `<p>foo</p>`,
+        fileName: 'myTemplate'
+      }
+    }
+  };
+  const { render } = courier(opts);
+
+  t.throws(() => render('template', []), message);
+  t.end();
+});
+
+test(`render should return a html string`, (t) => {
+  const template = () => (
+    <Email>
+      <Item>
+        <Span>Foo</Span>
+      </Item>
+    </Email>
+  );
+  const html = `<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">\n<html xmlns="http://www.w3.org/1999/xhtml">\n  \n  <head>\n    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />\n    <meta name="viewport" content="width=device-width, initial-scale=1.0"\n    />\n    <title></title>\n  </head>\n  \n  <body style="width:100%;margin:0;padding:0;-webkit-text-size-adjust:100%;-ms-text-size-adjust:100%;">\n    <table width="100%" height="100%" cellpadding="0" cellspacing="0" align="left"\n    valign="top">\n      <tbody>\n        <tr>\n          <td align="center" valign="top">\n            <table width="600" align="center" cellpadding="0" cellspacing="0" valign="top">\n              <tbody>\n                <tr>\n                  <td><span style="font-family:sans-serif;font-size:14px;line-height:14px;color:#000;">Foo</span>\n                  </td>\n                </tr>\n              </tbody>\n            </table>\n          </td>\n        </tr>\n      </tbody>\n    </table>\n  </body>\n\n</html>`;
+  const opts = {
+    allTemplates: {
+      myTemplate: {
+        template,
+        fileName: 'myTemplate'
+      }
+    }
+  };
+  const { render } = courier(opts);
+
+  t.equal(render('myTemplate'), html);
+  t.end();
+});
+
+test(`render should return a transformed mc:data string`, (t) => {
+  const template = () => (
+    <Email>
+      <Item>
+        <Span data-mc-edit='baz'>Foo</Span>
+      </Item>
+    </Email>
+  );
+  const html = `<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">\n<html xmlns="http://www.w3.org/1999/xhtml">\n  \n  <head>\n    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />\n    <meta name="viewport" content="width=device-width, initial-scale=1.0"\n    />\n    <title></title>\n  </head>\n  \n  <body style="width:100%;margin:0;padding:0;-webkit-text-size-adjust:100%;-ms-text-size-adjust:100%;">\n    <table width="100%" height="100%" cellpadding="0" cellspacing="0" align="left"\n    valign="top">\n      <tbody>\n        <tr>\n          <td align="center" valign="top">\n            <table width="600" align="center" cellpadding="0" cellspacing="0" valign="top">\n              <tbody>\n                <tr>\n                  <td><span mc:edit="baz" style="font-family:sans-serif;font-size:14px;line-height:14px;color:#000;">Foo</span>\n                  </td>\n                </tr>\n              </tbody>\n            </table>\n          </td>\n        </tr>\n      </tbody>\n    </table>\n  </body>\n\n</html>`;
+  const opts = {
+    allTemplates: {
+      myTemplate: {
+        template,
+        fileName: 'myTemplate'
+      }
+    }
+  };
+  const { render } = courier(opts);
+
+  t.equal(render('myTemplate'), html);
+  t.end();
+});
+
